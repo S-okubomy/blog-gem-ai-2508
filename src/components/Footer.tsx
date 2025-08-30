5f95426c738e265a27d34d47523187b7b43853fa
S-okubomy <okubo1504@gmail.com>
2025-08-30 16:46:37 +0900

    upd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-rose-100 text-rose-800 text-sm mt-auto container mx-auto px-6 py-4 text-center">
       <p className="font-semibold">
@@ -8,6 +10,9 @@ const Footer: React.FC = () => {
       </p>
       <p className="mt-2">
         楽しみながらご覧いただけると嬉しいです！掲載された情報のご利用は、ご自身の判断と責任でお願いいたします♪
+      </p>
+      <p className="mt-4 text-xs text-rose-600">
+        &copy;{currentYear} okbmsyk
       </p>
     </footer>
   );
